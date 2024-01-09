8c1d6f841c5508a8da7da7f3f810b4f5d37e3ff1
CeciCanedo <ceciliacanedolozada@gmail.com>
2024-01-09 14:06:23 -0600

    Estilos agregados a la pagina detalles

--- src/components/Detalles.js
+++ src/components/Detalles.js
@@ -13,15 +13,19 @@ const Detalles = () => {
 
   return (
     <div className="container">
-      <h4>Detalles de la Película: {movie.title}</h4>
+      <h4 className="titulo">Detalles de la Película: {movie.title}</h4>
+      
       <div className="card">
-        <img src={movie.poster_url} alt={movie.title} className="card-img-top" />
-        <div className="card-body">
+        
+        <div className="card-body cardContent">
+
           <h5 className="card-title">{movie.title}</h5>
+          <img src={movie.poster_url} alt={movie.title} className="card-img-top" />
           <p className="card-text">Resumen: {movie.overview}</p>
           <p className="card-text">Año de lanzamiento: {movie.release_year}</p>
-          <p className="card-text">ID: {movie.id}</p>
-          <p className="card-text">Votos: {movie.vote_average}</p>
+          <p className="card-text">Calificación: {movie.vote_average}</p>
+          <p className="card-text">Genero (id): {movie.genre_ids}</p>
+
         </div>
       </div>
     </div>
