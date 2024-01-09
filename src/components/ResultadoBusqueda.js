cf4b13834269e96505976ac59cd4b3e108f4691e
CeciCanedo <ceciliacanedolozada@gmail.com>
2024-01-09 13:33:58 -0600

    Estilos a pagina de resultados agregados

--- src/components/ResultadoBusqueda.js
+++ src/components/ResultadoBusqueda.js
@@ -10,57 +10,69 @@ const ResultadoBusqueda = () => {
   // URL del script PHP
   const URL = 'http://localhost/ApiPeticion.php';
 
+  //hacemos el fetch pasando como parametro el searchTerm que el usuario escribio en la barra de busqueda
   const fetchData = async (searchTerm) => {
     try {
       const response = await fetch(`${URL}?search=${searchTerm}`);
       const data = await response.json();
+      //confirmamos que los datos se estan recibiendo correctamente
       console.log(data);
-      setMovies(data); // Actualiza el estado con los datos recibidos del servidor PHP
+      // Actualiza el estado con los datos recibidos del servidor PHP
+      setMovies(data); 
       setShowMovie(true);
+      //Manejamos los errores si llegan a existir
     } catch (error) {
       console.error("Error al obtener los datos:", error);
     }
   };
 
+  
   useEffect(() => {
+    //Hacemos la consulta con el search del usuario
     const searchParams = new URLSearchParams(location.search);
     const searchTerm = searchParams.get('search');
     setSearch(searchTerm);
+    //confirmamos que exista un valor en searchTerm
     if (searchTerm) {
+      //Hacemos la solicitud 
       fetchData(searchTerm);
     }
   }, [location.search]);
 
+
+  //Renderizamos la pagina
   return (
     <div className="container">
-      <h4>Resultados de la búsqueda</h4>
-      <div className="card-container">
-        {showMovies &&
-          movies.map((movie, index) => (
-            <div className="card" key={index}>
-              {/*<img src={movie.poster_url} alt={movie.title} className="card-img-top" />*/}
-              <div className="card-body">
-                <h5 className="card-title">{movie.title}</h5>
-                <p className="card-text">Año de lanzamiento: {movie.release_year}</p>
-                <p className="card-text">ID: {movie.id}</p>
-                <p className="card-text">Votos: {movie.vote_average}</p>
-                {/*<p className="card-text">{movie.overview}</p>*/}
-                <Link
-  to={`/detalles/${movie.id}`} // Utiliza la ruta correcta para detalles de película
-  state={{ movieDetails: movie }}
-  className="btn btn-primary"
->
-  Ver Detalles
-</Link>
+      <h4 className="titulo">Resultados de la búsqueda</h4>
+      
+      <div className="row">
+        {showMovies && movies.map((movie, index) => (
+
+            <div className="col-md-4 mb-4" key={index}>
+
+              <div className="card">
+
+                <div className="card-body cardContent">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p className="card-text">Año de lanzamiento: {movie.release_year}</p>
+                  <p className="card-text">Votos: {movie.vote_average}</p>
+
+                  <Link to={`/detalles/${movie.id}`} state={{ movieDetails: movie }} className="btn btn-primary">
+                    Ver Detalles
+                  </Link>
+
+                </div>
               </div>
             </div>
           ))}
       </div>
+
       <div className="boton">
         <Link to="/" className="btn btn-primary" role="button">
           Volver a Inicio
         </Link>
       </div>
+
     </div>
   );
 };
